ebdd433cfa5374aecac9679c76173dd7eec3f861
ruslan <happierall@gmail.com>
2015-07-09 13:09:42 +0300

    Login auth doesn't work.

--- examples/example.go
+++ examples/example.go
@@ -9,15 +9,24 @@ import (
 func main() {
 	// Login/pass auth
 	var api = &vk_api.Api{}
-	err := api.LoginAuth(
-		"email/phone",
-		"pass",
-		"3087104",      // client id
-		"wall,offline", // scope (permissions)
-	)
-	if err != nil {
-		panic(err)
-	}
+    // ToDo need check captcha with  http://captchabot.com
+
+    // Temp get token
+    // Go To http://oauth.vk.com/oauth/authorize?redirect_uri=http://oauth.vk.com/blank.html&response_type=token&client_id=3087104&&v=5.0&scope=wall,offline&display=wap
+    // Login and copy token from url.
+    // And put token in the api.AccessToken
+
+    api.AccessToken = "put token here"
+
+//	err := api.LoginAuth(
+//		"email/pass",
+//		"pass",
+//		"3087104",      // client id
+//		"wall,offline", // scope (permissions)
+//	)
+//	if err != nil {
+//		panic(err)
+//	}
 
 	// Make query
 	params := make(map[string]string)
